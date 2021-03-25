61f935cb734909e7a618c351858e8b4ede9f2021
Sebastien Fourey <Sebastien.Fourey@ensicaen.fr>
2021-03-25 10:03:13 +0100

    First commit

---
+++ scripts/travis_build_qmake.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ev
+
+if [ "${TRAVIS_BRANCH}" = devel ]; then
+    config=debug
+else
+    config=release
+fi
+
+qmake --version
+
+echo "Building standalone plugin"
+qmake CONFIG+=${config} HOST=none GMIC_PATH=gmic-clone/src
+make
+
+echo "Building Gimp plugin"
+qmake CONFIG+=${config} HOST=gimp GMIC_PATH=gmic-clone/src
+make
+make
+
+echo "Building Krita plugin"
+qmake CONFIG+=${config} HOST=krita GMIC_PATH=gmic-clone/src
+make
+make
